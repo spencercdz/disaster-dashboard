d00aac6ffaf44b207baf9cb9ac368f06902b31bc
spencercdz <spencerchudz@gmail.com>
2025-06-03 18:02:28 +0800

    feat(search): improve search component layout and styling
    - Add new CSS classes for search input and button with hover effects
    - Restructure search component to use flex column layout
    - Adjust page layout to use fixed width columns instead of flex-grow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,21 +12,19 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <Header />
       
-      <div className="flex-1 justify-between md:flex relative">
-        <div className="flex justify-center flex-col flex-grow">
-          <div className="flex justify-center flex-col flex-grow">
+      <div className="flex-1 flex flex-row justify-between relative">
+        <div className="flex justify-center flex-col w-1/3">
+          <div className="flex justify-center flex-col flex-1">
             <Search />
             <Sentiment />
-          </div>
-          <div className="flex justify-center flex-col flex-grow">
             <Requests />
           </div>
         </div>
-        <div className="flex justify-center flex-col flex-grow">
+        <div className="flex justify-center flex-col w-1/3">
           <Map />
           <Chart />
         </div>
-        <div className="flex justify-center flex-col flex-grow">
+        <div className="flex justify-center flex-col w-1/3">
           <Tweets />
         </div>
       </div>
--- src/components/ContainerSearch.tsx
+++ src/components/ContainerSearch.tsx
@@ -1,16 +1,14 @@
 export default function Search() {
     return (
-        <div className = "flex container-default justify-between h-full">
-            <div>
+        <div className = "flex container-default flex-col h-full">
+            <div className="mb-2">
                 <h1 className="text-1xl font-bold">Search</h1>
             </div>
-            <div>
+            <div className="flex space-x-4">
                 <input type="text" placeholder="Country" className="search-input" />
                 <input type="text" placeholder="Disaster Type" className="search-input" />
                 <input type="text" placeholder="Year" className="search-input" />
-            </div>
-            <div>
-                <button className="search-button">Search</button>
+                <button className="search-button whitespace-nowrap">Search</button>
             </div>
         </div>
     )
