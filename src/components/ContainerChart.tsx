32f364ffcfbfefcb31b1b91e5b0d27650e5577d1
spencercdz <spencerchudz@gmail.com>
2025-06-05 15:00:27 +0800

    Added line

--- src/components/ContainerChart.tsx
+++ src/components/ContainerChart.tsx
@@ -1,8 +1,15 @@
+import { Line } from "react-chartjs-2";
+
 export default function Chart() {
+    
+
     return (
         <div className = "flex container-default justify-between h-full">
             <div>
                 <h1 className="text-1xl font-bold">Chart</h1>
+            </div>
+            <div>
+
             </div>
         </div>
     )
